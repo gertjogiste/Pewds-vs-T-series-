9e0a34f3d5a4d9951c5b3ebcf4b9be8eb239c89f
gertjogiste <gertjogiste@gmail.com>
2018-12-14 19:35:44 +0200

    panin muusika mängu sisse(intro)

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ res = [800, 640]
 Clock = pygame.time.Clock()
 pygame.init()
 
+pygame.mixer_music.load("Story(Hej Monika).wav")
+pygame.mixer_music.play(-1)
 player = Player(640, 400, 4, tilesize, tilesize/16*6)
 
 screen = pygame.display.set_mode(res)
