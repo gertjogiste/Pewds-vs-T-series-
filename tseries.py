8ac9e1e50b1a07dedb1d3d072495ae0c46f5516f
LordzShadow <44734448+LordzShadow@users.noreply.github.com>
2018-12-16 14:04:46 +0200

    stuff

--- tseries.py
+++ tseries.py
@@ -14,15 +14,18 @@ class TSeries:
         self.speedy = speed
         self.life = life
         self.image = pygame.image.load("images/t-series boss1.png")
+        self.health = 10
+        self.damage = 1
 
         self.normal1 = pygame.image.load("images/t-series boss1.png")
         self.normal1 = pygame.image.load("images/t-series boss2.png")
         self.death1 = pygame.image.load("images/t-series boss death1.png")
         self.death2 = pygame.image.load("images/t-series boss death2.png")
+        self.tilesize = tilesize
 
         self.moving_hor = True
 
-    def update(self, Maps, bullets, player):
+    def update(self, Maps, t_bullets, player, bullets):
         if 0 < self.y + self.speedy < res[1] - tilesize - self.image.get_rect().size[1]:
             self.y += self.speedy
         else:
@@ -35,7 +38,7 @@ class TSeries:
             self.x += self.speedx
 
         if self.shootcooldown == 100:
-            bullets.append(Bullet(self.shootpoint[0], self.shootpoint[1], tilesize, "Right", True, [player.x, player.y]))
+            t_bullets.append(Bullet(self.shootpoint[0], self.shootpoint[1], tilesize, "Right", True, [player.x, player.y]))
 
         self.shootpoint = [self.x + 90, self.y + 156]
 
@@ -43,6 +46,25 @@ class TSeries:
             self.shootcooldown -= 1
         else:
             self.shootcooldown = 100
+
+        for b, bullet in enumerate(bullets):
+            if self.x - self.tilesize / 2 < bullet.center[0] < self.x + self.tilesize / 2 \
+                    and self.y - self.tilesize < bullet.center[1] < self.y + self.tilesize:
+                del bullets[b]
+                if self.health > 0:
+                    if player.subs >= 5:
+                        self.health -= self.damage + 1
+                    elif player.subs >= 10:
+                        self.health -= self.damage + 2
+                    elif player.subs >= 15:
+                        self.health -= self.damage + 3
+                    elif player.subs >= 20:
+                        self.health -= self.damage + 4
+                    else:
+                        self.health -= self.damage
+
+        if self.health <= 0:
+            print(self.health)
 
     def render(self, screen, player):
         screen.blit(self.image, [self.x - player.x + 416, self.y])
